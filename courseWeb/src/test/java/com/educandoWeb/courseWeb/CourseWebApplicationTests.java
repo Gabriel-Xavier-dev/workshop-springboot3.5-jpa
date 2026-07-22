6868c2898a77e05444566ef928c86a7c74e9e59d
Gabriel-Xavier-dev <indieguy199777@gmail.com>
2026-07-22 01:21:24 -0300

    Project Created

---
+++ courseWeb/src/test/java/com/educandoWeb/courseWeb/CourseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoWeb.courseWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
